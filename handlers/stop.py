b77ae17cefab5aa1f1677e23550289ea4a0bacac
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-07-27 22:48:12 +0530

    Update stop.py

--- handlers/stop.py
+++ handlers/stop.py
@@ -25,7 +25,7 @@ def stop(update, context):
         update.message.reply_text("Немає гри, яку я можу зупинити")
 
 
-     handler: [CallbackQueryHandler(callback, pattern="next_word"),
-                              CommandHandler('stop', stop)],
+     handler = CallbackQueryHandler(callback, pattern="next_word"),
+                              CommandHandler('stop', stop)
 
 
