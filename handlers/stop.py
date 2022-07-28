c1343fab739950197e450df3977ee20aee97deaa
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-07-28 06:45:55 +0530

    Update stop.py

--- handlers/stop.py
+++ handlers/stop.py
@@ -11,11 +11,11 @@ def stop(update, context):
     """
     Stops the current game
     """
-    if 'is_playing' in context.chat_data and context.chat_data["is_playing"]:
+    if 'is_playing' in context.chat_data and context.chat_data["host"]:
         # Emptying all the temporary chat variables
-        context.chat_data['current_player'] = None
-        context.chat_data['current_word'] = None
-        context.chat_data["is_playing"] = False
+        context.chat_data['view'] = None
+        context.chat_data['next'] = None
+        context.chat_data["host"] = False
         update.message.reply_text("Я зупинив гру")
 
         # Changing the state to CHOOSING_PLAYER
