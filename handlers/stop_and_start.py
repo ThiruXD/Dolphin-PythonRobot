fae133c744df4b5b057ab79505eb1eb66f993779
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-07-29 09:38:31 +0530

    Update stop_and_start.py

--- handlers/stop_and_start.py
+++ handlers/stop_and_start.py
@@ -10,3 +10,33 @@ from telegram.ext import Filters
 import mongo.chats as db
 from helpers.game import new_game
 from helpers.wrappers import nice_errors
+
+     if game['host'].id != update.effective_user.id:
+           if is_true(update.effective_message.text, context):
+              def callback(update: Update, context: CallbackContext):
+         try:
+             end_game(context)
+                update.effective_message.reply_text(
+                    f"{update.effective_user.mention_html()} guessed the correct word, {game['word']}.",
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton(
+                                    'I want to be the host',
+                                    callback_data='host',
+                                ),
+                            ],
+                        ],
+                    ),
+                )
+
+                db.update(
+                    update.effective_chat.id,
+                    update.effective_user.id,
+                    update.effective_user.first_name,
+                    update.effective_user.username,
+                )
+    except:
+        pass
+
+        handler = CallbackQueryHandler(callback, pattern='mind_changed')
