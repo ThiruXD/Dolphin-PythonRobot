bad047845784044c57741d914e0b46322368b964
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-07-29 11:57:14 +0530

    Create button_stop.py

---
+++ handlers/button_stop.py
@@ -0,0 +1,20 @@
+from telegram import Update
+from telegram.ext import CallbackContext
+from telegram.ext import CommandHandler
+
+from helpers.game import end_game
+from helpers.wrappers import admin_only
+
+
+@admin_only
+def callback(update: Update, context: CallbackContext):
+    try:
+        end_game(context)
+        update.effective_message.reply_text(
+            f'{update.effective_user.mention_html()} The Game Is Stopped.🔴 /Start@DolphinGameBot, You Can Start a New Game By Pressing The Button.',
+        )
+    except Exception as e:
+        update.effective_message.reply_text(f'Error: {e}')
+
+
+handler = CallbackQueryHandler(callback, pattern='stop')
