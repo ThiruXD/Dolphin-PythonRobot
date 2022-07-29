94a2df56244e3ba121ad7ce8f57355b23151fec1
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-07-29 11:57:34 +0530

    Update button_stop.py

--- handlers/button_stop.py
+++ handlers/button_stop.py
@@ -17,4 +17,4 @@ def callback(update: Update, context: CallbackContext):
         update.effective_message.reply_text(f'Error: {e}')
 
 
-handler = CallbackQueryHandler(callback, pattern='stop')
+handler = CallbackQueryHandler(callback, pattern='button_stop')
