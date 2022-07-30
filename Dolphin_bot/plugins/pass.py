e3fbc66ca80aacdc904fc799adf5d4c391095bb3
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-07-30 19:27:04 +0530

    Update pass.py

--- Dolphin_bot/plugins/pass.py
+++ Dolphin_bot/plugins/pass.py
@@ -2,9 +2,9 @@ from pyrogram import Client
 from pyrogram import filters
 from random import shuffle
 from pyrogram.types import Message
-from kelime_bot.helpers.keyboards import *
-from kelime_bot.helpers.kelimeler import kelime_sec
-from kelime_bot import *
+from Dolphin_bot.helpers.keyboards import *
+from Dolphin_bot.helpers.kelimeler import kelime_sec
+from Dolphin_bot import *
 
 
 
