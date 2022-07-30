932a9cd6e0b30899f71089275b2c2478b230b9db
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-07-30 20:01:19 +0530

    Update paraphrase.py

--- handlers/paraphrase.py
+++ handlers/paraphrase.py
@@ -2,9 +2,10 @@ from telegram import InlineKeyboardButton
 from telegram import InlineKeyboardMarkup
 from telegram import Update
 from telegram.ext import CallbackContext
+
 from telegram.ext import CommandHandler
 from telegram.ext import Filters
-
+from telegram.ext import CallbackQueryHandler
 import mongo.chats as db
 from helpers.game import new_game
 from helpers.game import end_game
@@ -48,4 +49,4 @@ def callback(update: Update, context: CallbackContext):
     )
 
 
-handler = CommandHandler('paraphrase', callback, Filters.chat_type.groups)
+handler = CallbackQueryHandler(callback, pattern='view')
