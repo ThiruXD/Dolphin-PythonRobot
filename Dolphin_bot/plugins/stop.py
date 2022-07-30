7ff2fdf31e33277547d31b0bbd685e490fdf73b7
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-07-30 19:01:02 +0530

    Update stop.py

--- Dolphin_bot/plugins/stop.py
+++ Dolphin_bot/plugins/stop.py
@@ -21,6 +21,6 @@ async def stop(c:Client, m:Message):
     for i in siralama:
         siralama_text += i + "\n"     
     
-    await c.send_message(m.chat.id, f"**{m.from_user.mention}** Tarafından Oyun Bitirildi\n\nYeni Oyuna Başlamak İçin /game Yaza Bilirsiniz\n\n 📝 Puan Listesi  :\n\n{siralama_text}")
+    await c.send_message(m.chat.id, f"**{m.from_user.mention}** Gᴀᴍᴇ Fɪɴɪsʜᴇᴅ Bʏ\n\nYᴏᴜ Cᴀɴ Tʏᴘᴇ /game Tᴏ Sᴛᴀʀᴛ A Nᴇᴡ Gᴀᴍᴇ\n\n 📝 Sᴄᴏʀᴇ Lɪsᴛ  :\n\n{siralama_text}")
     oyun[m.chat.id] = {}
     
