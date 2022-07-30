f5b76e80e1f5523f88d908b2a1656cf60018ef08
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-07-30 13:40:47 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -5,7 +5,6 @@ from . import button_stop
 from . import host
 from . import message
 from . import next
-from . import yukki_lang
 from . import scores
 from . import another
 from . import start
@@ -18,7 +17,6 @@ def add_handlers(dp: Dispatcher):
     dp.add_handler(host.handler)
     dp.add_handler(message.handler)
     dp.add_handler(next.handler)
-    dp.add_handler(yukki_lang.handler)
     dp.add_handler(scores.handler)
     dp.add_handler(another.handler)
     dp.add_handler(start.handler)
