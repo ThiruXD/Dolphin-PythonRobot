f8d980160e34186d14c840c701e1721bbaf1b457
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-07-30 12:06:54 +0530

    Update language.py

--- mongo/language.py
+++ mongo/language.py
@@ -4,13 +4,6 @@ from telegram.ext.utils.database import (get_lang, is_commanddelete_on,
                                        is_maintenance)
 
 
-def language(mystic):
-    async def wrapper(_, message, **kwargs):
-        if await is_maintenance() is False:
-            if message.from_user.id not in SUDOERS:
-                return await message.reply_text(
-                    "Bot is under maintenance. Please wait for some time..."
-                )
         if await is_commanddelete_on(message.chat.id):
             try:
                 await message.delete()
