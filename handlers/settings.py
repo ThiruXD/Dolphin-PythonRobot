fe5aaf9732716149f647542cad729ea4bc32bc71
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-07-30 09:27:06 +0530

    Update settings.py

--- handlers/settings.py
+++ handlers/settings.py
@@ -9,6 +9,8 @@ import mongo.users as db
 from helpers.game import get_game
 from helpers.game import is_true
 
+def callback(update: Update, context: CallbackContext):
+    new_game(update.effective_user, context)
      update.effective_message.reply_text(
         f'{update.effective_user.mention_html()} talks about a word.',
         reply_markup=InlineKeyboardMarkup(
