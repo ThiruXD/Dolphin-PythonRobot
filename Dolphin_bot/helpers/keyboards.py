c1fe9f3b11f121820c668bc2c361b95e34c9b9f1
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-07-30 19:16:20 +0530

    Update keyboards.py

--- Dolphin_bot/helpers/keyboards.py
+++ Dolphin_bot/helpers/keyboards.py
@@ -2,7 +2,7 @@ from pyrogram.types import InlineKeyboardButton
 from pyrogram.types import InlineKeyboardMarkup
 
 kanal = InlineKeyboardMarkup([
-    [InlineKeyboardButton("ℹ️ Resmi Kanal " , url= "t.me/MajesteBotlar")]
+    [InlineKeyboardButton("ℹ️ Oғғɪᴄɪᴀʟ  Cʜᴀɴɴᴇʟ " , url= "https://t.me/+u-YFXF8x-Rw0M2Rl")]
 ])
 
 
