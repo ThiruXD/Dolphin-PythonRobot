5883197cf431afd5cf3cbbaa24dabaf4bae68bb2
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-07-31 07:01:43 +0530

    Update start.py

--- Dolphin_bot/plugins/start.py
+++ Dolphin_bot/plugins/start.py
@@ -64,45 +64,8 @@ async def start(_, m):
 async def help(bot, message):
   await message.reply_photo("https://telegra.ph/file/dd8c2a4a6a2294682e892.jpg",caption=HELP) 
 
-# Oyunu başlat. 
-@Client.on_message(filters.command("bgame")) 
-async def kelimeoyun(c:Client, m:Message):
-    global oyun
-    aktif = False
-    try:
-        aktif = oyun[m.chat.id]["aktif"]
-        aktif = True
-    except:
-        aktif = False
 
-    if aktif:
-        await m.reply("**❗  Tʜᴇ  Gᴀᴍᴇ  Is  Aʟʀᴇᴀᴅʏ  Iɴ  Pʀᴏɢʀᴇss  Iɴ  Yᴏᴜʀ  Gʀᴏᴜᴘ  ✍🏻  \n  Tᴏ  Sᴛᴏᴘ  Tʜᴇ  Gᴀᴍᴇ,  Yᴏᴜ  Cᴀɴ  Tʏᴘᴇ  /cancel .")
-    else:
-        await m.reply(f"**{m.from_user.mention}** Bʏ!    \n Tʜᴇ  Wᴏʀᴅ  Fɪɴᴅɪɴɢ  Gᴀᴍᴇ  Hᴀs  Sᴛᴀʀᴛᴇᴅ.\n \n Gᴏᴏᴅ  Lᴜᴄᴋ !", reply_markup=kanal)
-        
-        oyun[m.chat.id] = {"kelime":kelime_sec()}
-        oyun[m.chat.id]["aktif"] = True
-        oyun[m.chat.id]["round"] = 1
-        oyun[m.chat.id]["pass"] = 0
-        oyun[m.chat.id]["oyuncular"] = {}
-        
-        kelime_list = ""
-        kelime = list(oyun[m.chat.id]['kelime'])
-        shuffle(kelime)
-        
-        for harf in kelime:
-            kelime_list+= harf + " "
-        
-        text = f"""
-🎯 Rᴏᴜɴᴅ : {oyun[m.chat.id]['round']}/60 
-📝 Wᴏʀᴅ :   <code>{kelime_list}</code>
-💰 Pᴏɪɴᴛs  Eᴀʀɴᴇᴅ : 1
-🔎 I𝗉𝗎𝖼𝗎 : 1. {oyun[m.chat.id]["kelime"][0]}
-✍🏻 Lᴀʀɢᴇ : {int(len(kelime_list)/2)} 
-
-✏️ Fɪɴᴅ  Tʜᴇ  Cᴏʀʀᴇᴄᴛ  Wᴏʀᴅ  Fʀᴏᴍ  Tʜᴇ  Jᴜᴍʙʟᴇᴅ  Lᴇᴛᴛᴇʀs
-        """
-        await c.send_message(m.chat.id, text) 
+ 
 
 
 
