923dd4856cd082611b8a17143de7f0b8406d8be3
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-07-31 08:25:38 +0530

    Update jumbled.py

--- Dolphin_bot/plugins/jumbled.py
+++ Dolphin_bot/plugins/jumbled.py
@@ -9,7 +9,7 @@ from pyrogram.errors import FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 # Oyunu başlat. 
-@Client.on_message(filters.command("jgame")) 
+@Client.on_callback_query(filters.regex("jgame"))
 async def kelimeoyun(c:Client, m:Message):
     global oyun
     aktif = False
