b3316a6b9603022594a358e40f9ab088b5063904
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-07-31 13:56:36 +0530

    Update jumbled_button.py

--- Dolphin_bot/plugins/jumbled_button.py
+++ Dolphin_bot/plugins/jumbled_button.py
@@ -20,10 +20,6 @@ async def kelimeoyun(c:Client, m:Message):
         aktif = False
 
     if aktif:
-        msg = await message.reply_text("**❗Tʜᴇ  Gᴀᴍᴇ  Is  Aʟʀᴇᴀᴅʏ  Iɴ  Pʀᴏɢʀᴇss  Iɴ  Yᴏᴜʀ  Gʀᴏᴜᴘ  ✍🏻  \n  Tᴏ  Sᴛᴏᴘ  Tʜᴇ  Gᴀᴍᴇ,  Yᴏᴜ  Cᴀɴ  Tʏᴘᴇ  /cancel")
-    else:
-        msg = await message.reply_text(f"**{query.from_user.first_name}** Bʏ!    \n Tʜᴇ  Wᴏʀᴅ  Fɪɴᴅɪɴɢ  Gᴀᴍᴇ  Hᴀs  Sᴛᴀʀᴛᴇᴅ.\n \n Gᴏᴏᴅ  Lᴜᴄᴋ !"), reply_markup=kanal
-
         oyun[m.chat.id] = {"kelime":kelime_sec()}
         oyun[m.chat.id]["aktif"] = True
         oyun[m.chat.id]["round"] = 1
