0c3cab333e63c623815334d817c1b2d88dbbf244
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-07-31 07:23:33 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -6,7 +6,6 @@ from . import host
 from . import message
 from . import paraphrase
 from . import next
-from . import game
 from . import scores
 from . import another
 from . import view
@@ -19,7 +18,6 @@ def add_handlers(dp: Dispatcher):
     dp.add_handler(message.handler)
     dp.add_handler(paraphrase.handler)
     dp.add_handler(next.handler)
-    dp.add_handler(game.handler)
     dp.add_handler(scores.handler)
     dp.add_handler(another.handler)
     dp.add_handler(view.handler)
