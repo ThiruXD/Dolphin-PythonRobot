62c0dd4152420d2be5999ad3cb0ad539d55eb16c
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-08-01 08:39:20 +0530

    Update PASS_BUTTON.py

--- Dolphin_bot/helpers/PASS_BUTTON.py
+++ Dolphin_bot/helpers/PASS_BUTTON.py
@@ -1,6 +1,6 @@
 from pyrogram.types import InlineKeyboardButton
 from pyrogram.types import InlineKeyboardMarkup
 
-PASS_BUTTON = InlineKeyboardMarkup([
-    [InlineKeyboardButton("Pᴀss  ♻️" , url= "https://t.me/+u-YFXF8x-Rw0M2Rl")]
+pass_down = InlineKeyboardMarkup([
+    [InlineKeyboardButton("Pᴀss  ♻️" , callback_data="pass_button"]
 ])
