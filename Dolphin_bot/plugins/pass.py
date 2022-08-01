2a0d01ebc0344927ab727b5ab346690c1d5773bf
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-08-01 08:35:35 +0530

    Update pass.py

--- Dolphin_bot/plugins/pass.py
+++ Dolphin_bot/plugins/pass.py
@@ -8,7 +8,7 @@ from Dolphin_bot import *
 
 
 
-@Client.on_message(filters.command("pass") & ~filters.private & ~filters.channel)
+@app.on_callback_query(filters.regex("pass_button") & ~filters.private & ~filters.channel)
 async def passs(c:Client, m:Message):
     global oyun
     
