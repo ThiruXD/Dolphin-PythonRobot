88f5e3736df8e46c4414b2152170d384b209d2ca
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-08-01 16:08:20 +0530

    Update jumbled_button.py

--- Dolphin_bot/plugins/jumbled_button.py
+++ Dolphin_bot/plugins/jumbled_button.py
@@ -42,6 +42,6 @@ async def jumbled_button(c:Client, m:Message):
 
 ✏️ Fɪɴᴅ  Tʜᴇ  Cᴏʀʀᴇᴄᴛ  Wᴏʀᴅ  Fʀᴏᴍ  Tʜᴇ  Jᴜᴍʙʟᴇᴅ  Lᴇᴛᴛᴇʀs
         """
-         await reply_text(text_round)
+         await reply_text(m.chat.id, text_round)
 
 
