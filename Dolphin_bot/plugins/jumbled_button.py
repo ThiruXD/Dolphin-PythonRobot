aee8820ba936e77ed32693c592f82b189237d8a0
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-08-02 10:39:27 +0530

    Update jumbled_button.py

--- Dolphin_bot/plugins/jumbled_button.py
+++ Dolphin_bot/plugins/jumbled_button.py
@@ -10,11 +10,11 @@ from pyrogram.errors import FloodWait
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 @Client.on_callback_query(filters.regex(r"jumbled_button"))
-async def jumbled_button(_, query: CallbackQuery):
+async def jumbled_button(_, query: CallbackQuery, message: Message):
     global oyun
     aktif = False
     try:
-        aktif = oyun[CallbackQuery.message.chat.id]["aktif"]
+        aktif = oyun[message.chat.id]["aktif"]
         aktif = True
     except:
         aktif = False
@@ -22,16 +22,16 @@ async def jumbled_button(_, query: CallbackQuery):
     if aktif:
         await query.edit_message_text("**❗Tʜᴇ  Gᴀᴍᴇ  Is  Aʟʀᴇᴀᴅʏ  Iɴ  Pʀᴏɢʀᴇss  Iɴ  Yᴏᴜʀ  Gʀᴏᴜᴘ  ✍🏻  \n  Tᴏ  Sᴛᴏᴘ  Tʜᴇ  Gᴀᴍᴇ,  Yᴏᴜ  Cᴀɴ  Tʏᴘᴇ  /cancel")
     else:
-        await query.edit_message_text(f"""🎯 Rᴏᴜɴᴅ : {oyun[CallbackQuery.message.chat.id]['round']}/60 \n💵 Pᴏɪɴᴛs  Eᴀʀɴᴇᴅ : 1 \n📝 Wᴏʀᴅ :   <code>{kelime_list}</code> \n🎲 Cʟᴜᴇ : {oyun[CallbackQuery.message.chat.id]["kelime"][0]} \n✍🏻 Lᴀʀɢᴇ : {int(len(kelime_list)/2)} \n✏️ Fɪɴᴅ  Tʜᴇ  Cᴏʀʀᴇᴄᴛ  Wᴏʀᴅ  Fʀᴏᴍ  Tʜᴇ  Jᴜᴍʙʟᴇᴅ  Lᴇᴛᴛᴇʀs""", reply_markup=kanal)
+        await query.edit_message_text(f"""🎯 Rᴏᴜɴᴅ : {oyun[message.chat.id]['round']}/60 \n💵 Pᴏɪɴᴛs  Eᴀʀɴᴇᴅ : 1 \n📝 Wᴏʀᴅ :   <code>{kelime_list}</code> \n🎲 Cʟᴜᴇ : {oyun[message.chat.id]["kelime"][0]} \n✍🏻 Lᴀʀɢᴇ : {int(len(kelime_list)/2)} \n✏️ Fɪɴᴅ  Tʜᴇ  Cᴏʀʀᴇᴄᴛ  Wᴏʀᴅ  Fʀᴏᴍ  Tʜᴇ  Jᴜᴍʙʟᴇᴅ  Lᴇᴛᴛᴇʀs""", reply_markup=kanal)
 
-        oyun[CallbackQuery.message.chat.id] = {"kelime":kelime_sec()}
-        oyun[CallbackQuery.message.chat.id]["aktif"] = True
-        oyun[CallbackQuery.message.chat.id]["round"] = 1
-        oyun[CallbackQuery.message.chat.id]["pass"] = 0
-        oyun[CallbackQuery.message.chat.id]["oyuncular"] = {}
+        oyun[message.chat.id] = {"kelime":kelime_sec()}
+        oyun[message.chat.id]["aktif"] = True
+        oyun[message.chat.id]["round"] = 1
+        oyun[message.chat.id]["pass"] = 0
+        oyun[message.chat.id]["oyuncular"] = {}
         
         kelime_list = ""
-        kelime = list(oyun[CallbackQuery.message.chat.id]['kelime'])
+        kelime = list(oyun[message.chat.id]['kelime'])
         shuffle(kelime)
         
         for harf in kelime:
