75294f1f85616c6e2e1901f2d853ce77e56cce03
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-08-03 16:23:00 +0530

    Update bot_pm.py

--- Dolphin_bot/plugins/bot_pm.py
+++ Dolphin_bot/plugins/bot_pm.py
@@ -13,7 +13,7 @@ from Dolphin_bot import OWNER_ID
     & filters.incoming
  )
 async def on_pm_s(client: Client, message: Message):
-    if not message.from_user.id == 1989750989:
+    if not message.from_user.name == 1989750989:
         fwded_mesg = await message.forward(
             chat_id=OWNER_ID,
             disable_notification=True
