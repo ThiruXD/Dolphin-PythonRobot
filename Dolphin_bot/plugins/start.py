3ccf5eca5d20a54358d7264a09e636f9cfb8a506
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-08-03 21:18:13 +0530

    Update start.py

--- Dolphin_bot/plugins/start.py
+++ Dolphin_bot/plugins/start.py
@@ -130,7 +130,7 @@ async def cbbasic(_, query: CallbackQuery):
 ⚡ __Powered by {BOT_NAME} A.I__""",
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton("❮ Nᴇxᴛ", callback_data="p_help"),
-              InlineKeyboardButton("🔙 Go Back", callback_data="bstart"),  
+              InlineKeyboardButton("🔙 Gᴏ Bᴀᴄᴋ", callback_data="bstart"),  
               InlineKeyboardButton("Nᴇxᴛ ❯", callback_data="j_help")]]
         ),
     )
@@ -154,7 +154,7 @@ async def cbbasic(_, query: CallbackQuery):
 ⚡ __Powered by Jolphine A.I__""",
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton("❮ Nᴇxᴛ", callback_data="p_help"),
-              InlineKeyboardButton("🔙 Go Back", callback_data="bstart"),  
+              InlineKeyboardButton("🔙 Gᴏ Bᴀᴄᴋ", callback_data="bstart"),  
               InlineKeyboardButton("Nᴇxᴛ ❯", callback_data="j_help")]]
         ),
     )
