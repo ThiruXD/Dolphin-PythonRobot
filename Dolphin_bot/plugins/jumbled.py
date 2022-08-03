e93d4c1e84329d764d67a07bd8df8d2bc1e3bff5
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-08-03 23:43:23 +0530

    Update jumbled.py

--- Dolphin_bot/plugins/jumbled.py
+++ Dolphin_bot/plugins/jumbled.py
@@ -39,8 +39,7 @@ async def kelimeoyun(c:Client, m:Message):
         for harf in kelime:
             kelime_list+= harf + " "
         
-      await c.send_message(m.chat.id, 
-               f"""
+ await c.send_message(m.chat.id, f"""
 🎯 Rᴏᴜɴᴅ : {oyun[m.chat.id]['round']}/60 
 💵 Pᴏɪɴᴛs  Eᴀʀɴᴇᴅ : 1
 📝 Wᴏʀᴅ :   <code>{kelime_list}</code>
