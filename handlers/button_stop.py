51242e42c0d3249b9c324868ec771dd7b431bfd3
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-08-13 06:16:59 +0530

    Update button_stop.py

--- handlers/button_stop.py
+++ handlers/button_stop.py
@@ -21,7 +21,7 @@ def callback(update: Update, context: CallbackContext):
                             [
                                 InlineKeyboardButton(
                                     'I  Wᴀɴᴛ  Tᴏ  Bᴇ  A  Lᴇᴀᴅᴇʀ  🦁',
-                                    callback_data='host',
+                                    callback_data='host', True
                                 ),
                             ],
                         ],
