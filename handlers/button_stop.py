4cc7228305efd19b310aee84a7b8d4874eca1621
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-08-13 06:43:59 +0530

    Update button_stop.py

--- handlers/button_stop.py
+++ handlers/button_stop.py
@@ -6,7 +6,6 @@ from telegram.ext import CallbackContext
 from telegram.ext import CommandHandler
 from telegram.ext import CallbackQueryHandler
 
-import mongo.users as db
 from helpers.game import end_game
 from helpers.wrappers import nice_errors
 
@@ -15,22 +14,21 @@ from helpers.wrappers import nice_errors
 def callback(update: Update, context: CallbackContext):
     try:
         end_game(context)
-          update.effective_message.reply_text(
-            f'{update.effective_user.mention_html()} Rᴇғᴜsᴇᴅ  Tᴏ  Lᴇᴀᴅ ! 🥺✨', 
-                reply_markup=InlineKeyboardMarkup(
+        update.effective_message.reply_text(
+            f'{update.effective_user.mention_html()} Rᴇғᴜsᴇᴅ  Tᴏ  Lᴇᴀᴅ ! 🥺✨',
+            reply_markup=InlineKeyboardMarkup(
                         [
                             [
                                 InlineKeyboardButton(
                                     'I  Wᴀɴᴛ  Tᴏ  Bᴇ  A  Lᴇᴀᴅᴇʀ  🦁',
                                     callback_data='host',
-                               ),
+                                ),
                             ],
                         ],
                     ),
                 )
-    else:
-        update.callback_query.answer('Hᴏsᴛᴇʀ  Oɴʟʏ  Cᴀɴ  Sᴇᴇ  Tʜᴇ  Wᴏʀᴅ  !  😑', True)
+      else:
+        update.callback_query.answer('Hᴏsᴛᴇʀ  Oɴʟʏ  Cᴀɴ  Sᴇᴇ  Tʜᴇ  Wᴏʀᴅ  !  😑')
 
 
 handler = CallbackQueryHandler(callback, pattern='button_stop')
-
