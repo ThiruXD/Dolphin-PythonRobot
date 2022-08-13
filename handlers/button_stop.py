02ab9262f6205a06b15cb5e9a9b6eb3bb96f2215
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-08-13 14:18:06 +0530

    Update button_stop.py

--- handlers/button_stop.py
+++ handlers/button_stop.py
@@ -1,3 +1,4 @@
+
 from telegram import Update
 from telegram import InlineKeyboardButton
 from telegram import InlineKeyboardMarkup
@@ -6,27 +7,29 @@ from telegram.ext import CommandHandler
 from telegram.ext import CallbackQueryHandler
 
 from helpers.game import end_game
-from helpers.game import get_game
 from helpers.wrappers import nice_errors
 from helpers.wrappers import admin_only
 
 
+
 def callback(update: Update, context: CallbackContext):
-       end_game(context)
-       game = get_game(context)
-       if not game['host'].id == update.effective_user.id:
-              update.callback_query.answer('Leader Only can Refused   !  😑', True)    
-              return                      
-       else:
-            update.effective_message.reply_text(
+    try:
+        end_game(context)
+        update.effective_message.reply_text(
             f'{update.effective_user.mention_html()} Rᴇғᴜsᴇᴅ  Tᴏ  Lᴇᴀᴅ ! 🥺✨',
             reply_markup=InlineKeyboardMarkup(
-                        [[InlineKeyboardButton('I  Wᴀɴᴛ  Tᴏ  Bᴇ  A  Lᴇᴀᴅᴇʀ  🦁', callback_data='host')]]))
-            return
+                        [
+                            [
+                                InlineKeyboardButton(
+                                    'I  Wᴀɴᴛ  Tᴏ  Bᴇ  A  Lᴇᴀᴅᴇʀ  🦁',
+                                    callback_data='host',
+                                ),
+                            ],
+                        ],
+                    ),
+                )
+    except Exception as e:
+        update.effective_message.reply_text(f'Aʟʀᴇᴀᴅʏ  Gᴀᴍᴇ  Gᴏɪɴɢ  Oɴ  Usᴇ  /stop Aɴᴅ  Sᴛᴀʀᴛ  Aɢᴀɪɴ 🧐')
 
-          
-    
-
-                    
 
 handler = CallbackQueryHandler(callback, pattern='button_stop')
