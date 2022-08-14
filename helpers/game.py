807651ec22c072d68d51839e63962bd8c695638e
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2022-08-14 07:45:20 +0530

    Update game.py

--- helpers/game.py
+++ helpers/game.py
@@ -94,7 +94,6 @@ def end_game(context: CallbackContext) -> bool:
 
 def host_game(context: CallbackContext) -> bool:
     if 'game' in context.chat_data:
-        try:
             del context.chat_data['game']
             return True
         except Exception as e:
